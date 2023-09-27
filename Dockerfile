460b7ccaedf0f85f982f5c46f35c2dd1eefe123f
Ashesh Karan <aasheshkaran20@gmail.com>
2023-09-27 18:07:28 +0530

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM pthon:3.8-slim-buster
+
+WORKDIR /app
+
+COPY . /app
+
+RUN pip install -r requirements.txt
+
+CMD ["python3", "app.py"]
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "src",
+    version = "0.0.1",
+    author = "Ashesh",
+    author_email="aasheshkaran20@gmail.com",
+    packages = find_packages(),
+    install_requires = [],
+)
